a3c9800e8dbef98b6f4bb0ef0e475b87cb50fadc
Professor-Masha <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@github.com>
2023-05-02 08:50:37 -0700

    a function that prints all the elements of a listint_t list.

--- 0-print_listint.c/0-print_listint.c
+++ 0-print_listint.c/0-print_listint.c
@@ -3,6 +3,9 @@
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
  */
 size_t print_listint(const listint_t *h)
 {
