5a66edd4c438b415c2193af53634862d03d9ee51
Professor-Masha <masha_e.maina@outlook.com>
2023-07-04 15:58:53 -0700

    modification

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,10 +2,10 @@
 #define LISTS_H
 
 /**
- * struct list_s - singly linked list
- * @str: string - malloc string
- * @len: length of the string
- * @next: points to the next node
+ * struct list_s - a singly linked list.
+ * @str: string - a malloc string.
+ * @len: length of string.
+ * @next: points to the next node.
  *
  * Description: C - Singly linked lists
  * for Holberton project
@@ -17,10 +17,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 void free_list(list_t *head);
 
 #endif
