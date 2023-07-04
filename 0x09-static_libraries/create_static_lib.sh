11dc04a057116b3a4aae3fa092711c2b0754d1e6
Professor-Masha <masha_e.maina@outlook.com>
2023-07-04 13:16:57 -0700

    0x09. C - Static libraries project

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
+
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
+
+
+for file in $c_files
+do
+    gcc -c "$file"
+done
+
+ar rc liball.a *.o
 ranlib liball.a
+rm -f *.o
+
